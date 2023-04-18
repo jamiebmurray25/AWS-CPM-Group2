ce9a3d93cc51a6952ebcad00a7064280b94ca708
Jamie Murray <jamiebmurray25@gmail.com>
2023-04-18 16:49:57 +1000

    add css file to index

--- app/index.php
+++ app/index.php
@@ -3,6 +3,15 @@
     $x = "David is here";
 ?>  
 
-<h1><?php echo $test ?>
-<h1><?php echo $x ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <link rel="stylesheet" href="assets/index.css">
+</head>
+<body>
 
+<h1><?php echo $test ?></h1>
+<h1><?php echo $x ?></h1>
+
+</body>
+</html>
