fa4727910dfb162b1036a16b11467bcc97700a16
Jamie Murray <jamiebmurray25@gmail.com>
2023-04-22 00:20:28 +1000

    add ability to search by customer_id

--- app/index.php
+++ app/index.php
@@ -3,24 +3,29 @@ $servername = "localhost";
 $username = "root";
 $dbname = "myDB";
 
-// Create connection
 $conn = new mysqli($servername, $username, null, $dbname);
 
-// Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "SELECT * FROM customer";
+$sql = "SELECT * FROM customer WHERE customer_id = 1";
 
 $result = $conn->query($sql);
-?>
 
+if ($result->num_rows) {
+  $row = $result->fetch_assoc();
+} else {
+  echo "No customer found for that ID";
+  die();
+}
+
+?>
 
 <ol>
   <?php
-  while ($row = $result->fetch_assoc()) {
-    echo "<li>" .  $row["first_name"] . "</li>";
+  foreach ($row as $column) {
+    echo "<li>" . $column . "</li>";
   }
   ?>
 </ol>
