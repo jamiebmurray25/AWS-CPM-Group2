0daa02ffd73c167655e4b5c000e113e51e0b8eb0
davidangga <davidanggadeveloper@gmail.com>
2023-04-28 23:08:00 +1000

    Making an functional delete and add client

--- app/index.php
+++ app/index.php
@@ -8,14 +8,39 @@ try{
     }
     }
 catch (Exception $e){
-    throw new Exception($e->getMessage());
+    echo $e->getMessage();
 }
 
 function selectAll($conn){
   $results = $conn -> query("SELECT * FROM customer") -> fetch_all(MYSQLI_ASSOC);
+  mysqli_close($conn);
   return $results;
 }
 
+function deleteClient($conn, $id){
+  if (mysqli_query($conn, "DELETE FROM customer WHERE customer_id = {intval($id)}")){
+  }
+  else{
+    echo mysqli_error($conn);
+  }
+}
+
+function addClient($conn){
+  $sql = "INSERT INTO customer (first_name, last_name, email, phone_number, risk_profile, portfolio_assessed) VALUES ('{$_POST['first-name']}', '{$_POST['last-name']}', '{$_POST['email']}', '{$_POST['phone']}', '{$_POST['risk-profile']}', false)";
+  mysqli_query($conn, $sql);
+}
+
+if (isset($_POST["action"])){ 
+  switch ($_POST["action"]) {
+    case "delete":
+      deleteClient($conn,$_POST["customerId"]);
+      break;
+    case "add":
+      addClient($conn);
+    default:
+      break;
+  }
+}
 ?>
 
 <!-- HTML css and js -->
@@ -26,7 +51,7 @@ function selectAll($conn){
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Dashboard</title>
-  <link rel="stylesheet" href="assets/css/index.css">
+  <link rel="stylesheet" href="assets/css/index.css?v=1234">
 </head>
 <body>
 
@@ -51,14 +76,10 @@ function selectAll($conn){
           <label for="risk-profile">Risk Profile:</label>
           <select id="risk-profile" name="risk-profile" required>
             <option value="">--Select--</option>
-            <option value="low">Low</option>
-            <option value="medium">Medium</option>
-            <option value="high">High</option>
+            <option value="Low Risk">Low</option>
+            <option value="Medium Risk">Medium</option>
+            <option value="High Risk">High</option>
           </select>
-
-          <label for="portfolio-value">Portfolio Value:</label>
-          <input type="number" id="portfolio-value" name="portfolio-value" required>
-
           <button type="submit">Submit</button>
         </form>
     </div>
@@ -92,13 +113,11 @@ function selectAll($conn){
       <table class="styled-table">
         <thead>
             <tr>
-                <th>ID</th>
                 <th>First Name</th>
                 <th>Last Name</th>
                 <th>Email</th>
                 <th>Phone Number</th>
                 <th>Risk Profile</th>
-                <th>Portfolio Value</th>
                 <th>Portfolio Assessed</th>
                 <th></th>
             </tr>
@@ -109,19 +128,27 @@ function selectAll($conn){
             foreach ($results as $row) {
               $clicklocation = "location.href='customers.php?id={$row['customer_id']}'";
               $assessed = "Not Yet";
+              $assessedclr = "red";
+              $riskclr = "green";
               if ($row["portfolio_assessed"]){
                 $assessed = "Done";
+                $assessedclr = "green";
+              }
+
+              if ($row["risk_profile"] == "High Risk"){
+                $riskclr = "red";
+              }
+              elseif($row["risk_profile"] == "Medium Risk"){
+                $riskclr = "orange";
               }
 
               echo "<tr>";
-              echo "<td onclick={$clicklocation}>{$row['customer_id']}</td>";
               echo "<td onclick={$clicklocation}>{$row['first_name']}</td>";
               echo "<td onclick={$clicklocation}>{$row['last_name']}</td>";
               echo "<td onclick={$clicklocation}>{$row['email']}</td>";
               echo "<td onclick={$clicklocation}>{$row['phone_number']}</td>";
-              echo "<td onclick={$clicklocation}>{$row['risk_profile']}</td>";
-              echo "<td onclick={$clicklocation}>{$row['portfolio_value']}</td>";
-              echo "<td onclick={$clicklocation}>{$assessed}</td>";
+              echo "<td onclick={$clicklocation} style='color:{$riskclr}'>{$row['risk_profile']}</td>";
+              echo "<td onclick={$clicklocation} style='color:{$assessedclr}'>{$assessed}</td>";
               echo "<td><button id='delete-btn' onclick='openDeleteConfirmation({$row['customer_id']})'>Delete</button></td>";
               echo "</tr>";
             }
